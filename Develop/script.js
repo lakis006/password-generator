1435eec787434ea337653a6df8278595e0ca390b
Jsquad <lakis006@umn.edu>
2020-04-18 22:06:54 -0500

    added a generate password function, randomized password inputs, and set conditions for the generate passowrd parameters

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,9 @@
 var generateBtn = document.querySelector("#generate");
 
 
+//randMaker will take two paramiters ( a , b ) => 
+const randMaker = (a ,b) => { return Math.floor((Math.random() * b)  + a);} 
+
 
 // Write password to the #password input
 function writePassword() {
@@ -22,10 +25,11 @@ var password = "";
   // These lines (23 & 24) are checking if at least 1 characteristic is true
     var acceptable = (numbers || upperCase || lowerCase || specialCharacters );
     if (acceptable) {
-       password = generatePassword();
+      password = generatePassword(numbers, upperCase, lowerCase, specialCharacters, length);
     } else {
       alert("You need at least 1 acceptable criteria");
     }
+
 
  
   var passwordText = document.querySelector("#password");
@@ -33,7 +37,65 @@ var password = "";
   // This gets printed out on the html
   passwordText.value = password;
 
+  console.log(password.length);
 }
+
+
+// arguements can be integers(number) text, functions, variables etc..
+
+
+
+function generatePassword(numbers, upperCase, lowerCase, specialCharacters, length) {
+
+var spChr = "!@#$%^&*()_-=+~`|}{][\/?<>;:.,'" + '"';
+var upCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+var lcase = "qwertyuiopasdfghjklzxcvbnm";
+var num = "1234567890";
+//code
+  console.log(numbers);
+  console.log(lowerCase);
+  console.log(upperCase);
+  console.log(specialCharacters);
+  console.log(length);
+  var x = randMaker(0,spChr.length);
+  console.log(x);
+  console.log(spChr[x]);
+
+
+ 
+
+
+  var theBox = "";
+ for (var i = 0; i < length;) {
+   if (numbers && i < length) {
+    theBox += num[randMaker(0,10)];
+    i++; 
+   }
+
+   if (upperCase && i < length ) {
+    theBox += upCase[randMaker(0, upCase.length)];
+    i++;
+   }
+
+   if (specialCharacters && i < length) {
+    theBox += spChr[randMaker(0, spChr.length)];
+    i++; 
+   }
+
+   if (lowerCase && i < length ) {
+    theBox += lcase[randMaker(0, lcase.length)];
+    i++; 
+   }
+ }
+
+ return theBox;
+ 
+ 
+};
+  
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,27 +1,5 @@
 # password-generator
 
-GIVEN I need a new, secure password
-
-1.) WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-
-2.) WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-
-3.) WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters 
-
-4.) WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-
-5.) WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-
-6.) WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-
+In this assignment, the goal was to randomly generate a password that would render for the user after having answered a series of prompts. The password would have a certain length, be random, and would react with how the user chooses their preference of random charaters, numbers, lowercase, and uppercase letters. Finally, the password would be generated after all the prompts have been fulfilled.
 
 
